24f822267cebb16411d58e97bbeb1204ce9d4927
krasi <krasimir.xristov@gmail.com>
2025-01-05 09:29:00 +0100

    Create footer

---
+++ components/shared/footer.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import { ChevronUp } from 'lucide-react'
+import { Button } from '../ui/button'
+import Link from 'next/link'
+import { APP_NAME } from '@/lib/constants'
+
+const Footer = () => {
+  return (
+    <footer className='bg-black text-white underline-link'>
+      <div className='w-full'>
+        <Button
+          variant='ghost'
+          className='bg-gray-800 w-full rounded-none'
+          onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+        >
+          Back to top
+        </Button>
+      </div>
+      {/* Footter links */}
+      <div className='p-4'>
+        <div className='flex justify-center gap-3 text-sm'>
+          <Link href='/page/conditions-of-use'>Conditions of Use</Link>
+          <Link href='/page/privacy-policy'>Privacy Notice</Link>
+          <Link href='/page/help'>Help</Link>
+        </div>
+        <div className='flex justify-center text-sm'>
+          <p>© 2025 {APP_NAME}, Inc. or its affiliates</p>
+        </div>
+        <div className='mt-8 flex justify-center text-sm text-gray-400'>
+          {' '}
+          123, Main Street, Anytown, CA, Zip 12345 | +0 (123) 456-7890
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
