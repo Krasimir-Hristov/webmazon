dacab952ae7571d08d18b814514a071236bcb2ad
krasi <krasimir.xristov@gmail.com>
2025-01-08 18:32:30 +0100

    05-connect-to-mongodb-and-seed-products

---
+++ lib/db/seed.ts
@@ -0,0 +1,24 @@
+import { cwd } from 'process'
+import { loadEnvConfig } from '@next/env'
+import data from '../data'
+import { connectToDatabase } from '.'
+import Product from './models/product.model'
+
+loadEnvConfig(cwd())
+
+const main = async () => {
+  try {
+    const { products } = data
+    await connectToDatabase(process.env.MONGODB_URI)
+
+    await Product.deleteMany()
+    const createdProducts = await Product.insertMany(products)
+
+    console.log({ createdProducts, message: 'Seed data successfully imported' })
+  } catch (error) {
+    console.error('Seed data import failed')
+    throw new Error('Failde to seed data')
+  }
+}
+
+main()
