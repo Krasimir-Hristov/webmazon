39831fc96fda4881e0acba8c272ffb384a3170ed
krasi <krasimir.xristov@gmail.com>
2025-01-09 12:50:15 +0100

    add some new chages

--- components/shared/product/ProductSlider.tsx
+++ components/shared/product/ProductSlider.tsx
@@ -6,7 +6,7 @@ import {
   CarouselItem,
 } from '@/components/ui/carousel'
 import { IProduct } from '@/lib/db/models/product.model'
-import ProductCard from './productCard'
+import ProductCard from './ProductCard'
 
 const ProductSlider = ({
   titel,
